97ae409a836ce4745e6488e4750e97050ba9ca2c
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-09-06 15:05:18 +0200

    Communicate from the ChildComponent to the ParentComponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container">
-      <SearchBar></SearchBar>
-    </div>
-  );
-};
+class App extends React.Component {
+
+  onFormSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onSubmit={this.onFormSubmit}></SearchBar>
+      </div>
+    );
+  }
+}
 
 export default App;
