3f40b755afa1c3d44548a4ece6dca574e5f596f9
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2019-10-15 16:24:15 +0200

    Implemented ImageList

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
@@ -17,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onFormSubmit}></SearchBar>
-        Found {this.state.images.length} images
+        <ImageList images={this.state.images}></ImageList>
       </div>
     );
   }
